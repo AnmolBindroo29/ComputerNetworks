dfc59d474563dcb7b72ee0cb38353d43ab7d588e
Anmol Bindroo <bindrooa19.comp@coep.ac.in>
2021-08-26 18:29:10 +0530

    updates

--- client.py
+++ client.py
@@ -1,16 +1,34 @@
+import sys
 import threading
 from socket import *
 serverName = '127.0.0.1'
-serverPort = 12000
-nickname = input("Choose a nickname: ")
+serverPort = int(sys.argv[1])
+stop_thread = False
+nickname = sys.argv[2]
+if nickname == 'admin':
+	passwrd = input("Enter password")
 client = socket(AF_INET, SOCK_STREAM)
 client.connect((serverName, serverPort))
 def recieve():
 	while True:
+		global stop_thread 
+		if(stop_thread):
+			break
 		try:
 			message = client.recv(1024).decode('ascii')
 			if message == 'NICK':
 				client.send(nickname.encode('ascii'))
+				next_msg  = client.recv(1024).decode('ascii')
+				if next_msg == 'PASS':
+					client.send(passwrd.encode('ascii'))
+					if(client.recv(1024).decode('ascii') == 'REFUSE'):
+						print("Connection Refused")
+						stop_thread = True
+				elif next_msg == 'BAN':
+					print("Connection refused because of ban!")
+					client.close()
+					stop_thread = True
+
 			else:
 				print(message)
 		except:
@@ -19,10 +37,22 @@ def recieve():
 			break
 def write():
 	while True:
+		if(stop_thread):
+			break
 		message = f'{nickname}:{input("")}'
-		client.send(message.encode('ascii'))
+		if message[len(nickname) +2:].startswith('/') and nickname == 'admin':
+			if message[len(nickname) + 2:].startswith('/kick'):
+				#print("")
+				client.send(f'KICK {message[len(nickname)+2+6:]}'.encode('ascii'))
+			elif message[len(nickname) + 2:].startswith('/ban'):
+				client.send(f'BAN {message[len(nickname)+7:]}'.encode('ascii'))
+			else:
+				print("Invalid Operation")
+		else:
+			client.send(message.encode('ascii'))
 recieve_thread = threading.Thread(target = recieve)
 recieve_thread.start()
 
 write_thread = threading.Thread(target = write)
 write_thread.start()
+ 
--- server.py
+++ server.py
@@ -1,10 +1,11 @@
 import threading
+import sys
 from socket import *
-serverPort = 12000
+serverPort = int(sys.argv[1])
 localhost = '127.0.0.1'
 server = socket(AF_INET, SOCK_STREAM)
 server.bind((localhost, serverPort))
-server.listen(1)
+server.listen()
 print("Server Ready")
 clients = []
 nicknames = []
@@ -12,11 +13,36 @@ def broadcast(message):
 	for client in clients:
 		client.send(message)
 
+def kick_user(name):
+	if name in nicknames:
+		index = nicknames.index(name)
+		client_to_kick = clients[index]
+		clients.remove(client_to_kick)
+		client_to_kick.send("You were kicked by the admin".encode('ascii'))
+		client_to_kick.close()
+		nicknames.remove(name)
+		broadcast(str(name) + "was kicked by admin".encode('ascii'))
 def handle(client):
 	while True:
 		try:
-			message = client.recv(1024)
-			broadcast(message)
+			msg = client.recv(1024)
+			if msg.decode('ascii').startswith('KICK'):
+				if nicknames[clients.index(client) ] == 'admin':
+					name_to_kick = msg.decode('ascii')[5:]
+					kick_user(name_to_kick)
+				else:
+					clients.send("Command was refused!".encode('ascii'))
+			elif msg.decode('ascii').startswith('BAN'):
+				if nicknames[clients.index(client)] == 'admin':
+					name_to_ban = msg.decode('ascii')[4:]
+					ban_user(name_to_ban)
+					with open(ban.txt, 'a') as f:
+						f.write(name_to_ban+'\n')
+					print(name_to_ban, 'was banned!')
+				else:
+					client.send("Command was refused!".encode('ascii'))
+			else:
+				broadcast(msg)
 		except:
 			index = clients.index(client)
 			clients.remove(client)
@@ -32,11 +58,18 @@ def recieve():
 		print(f"Connected with {str(addr)}")
 		client.send('NICK'.encode('ascii'))
 		nickname = client.recv(1024).decode('ascii')
+		if nickname == 'admin':
+			client.send('PASS'.encode('ascii'))
+			msg = client.recv(1024).decode('ascii')
+			if msg != 'root@kali':
+				client.send('REFUSE'.encode('ascii'))
+				client.close()
+				continue
 		nicknames.append(nickname)
 		clients.append(client)
 		print(f"Nickname of the client is {nickname}!")
 		broadcast(f'{nickname} joined the chat!'.encode('ascii'))
-		client.send("Connected to the server!".encode('ascii'))
+		#client.send("Connected to the server!".encode('ascii'))
 		thread = threading.Thread(target = handle, args = (client,))
 		thread.start()
 print("Server is Listening...")
